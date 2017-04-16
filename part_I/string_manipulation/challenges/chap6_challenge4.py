b0c72fa1b53e5dce570ecfdbfe5b96cc9fe9bd99
Matt <mattbingham@users.noreply.github.com>
2017-04-16 21:12:32 +0200

    Update chap6_challenge4.py
    Solution incorrect, unsure of answer but thought I would give you a heads up!

--- part_I/string_manipulation/challenges/chap6_challenge4.py
+++ part_I/string_manipulation/challenges/chap6_challenge4.py
@@ -1,2 +1,7 @@
 lst = "Where now? Who now? When now?".split("?")
 print(lst)
+
+"""
+Not actually sure of the solution but this is incorrect as in your book (page 100, challenge 4), you state "return a list that looks like: 
+["Where now?", "Who now?", "When now?"] - but in actual fact the solution provided does not return the question marks.
+"""
