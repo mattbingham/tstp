2c7717dd51647ad028463a426ce6acae09890286
Matt <mattbingham@users.noreply.github.com>
2017-04-16 21:26:12 +0200

    Update chap6_challenge10.py
    Loses a line

--- part_I/string_manipulation/challenges/chap6_challenge10.py
+++ part_I/string_manipulation/challenges/chap6_challenge10.py
@@ -1,3 +1,2 @@
-sentence = "It was a bright cold day in April, and the clocks were striking thirteen."
-slce = sentence[0:33]
-print(slce)
+a = "It was a bright cold day in April, and the clocks were striking thirteen."
+print(a[:33])
